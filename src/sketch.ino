49a0662f383a18f4665ee213a92d0d3f4c22bafd
urthbound <jeffowler@gmail.com>
2015-03-01 21:20:59 -0500

    changing freq by talking over serial

--- src/sketch.ino
+++ src/sketch.ino
@@ -1,20 +1,33 @@
 long previousMicros = 0;
 long currentMicros;
 unsigned long interval = freq_to_micros(440.0);
+unsigned long new_interval = freq_to_micros(440.0);
 int out = LOW;
+
+int change = 0;
 
 void setup()
 {
     for (int i = 0; i < 8; i++) {
         pinMode(i, OUTPUT);
     }
+
+    Serial.begin(9600);
 }
 
 unsigned long freq_to_micros(float freq) {
     return (1000.0 / freq) * 1000.0;
 }
 
+void reset_interval() {
+    interval = freq_to_micros(Serial.parseFloat());
+}
+
 void square() {
+
+    if (Serial.available()) {
+        reset_interval();
+    }
 
     currentMicros = micros();
     if (currentMicros - previousMicros > interval) {
