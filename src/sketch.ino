f77a61eaeb6dd59848009b0b650e820891365a2c
urthbound <jeffowler@gmail.com>
2015-03-03 19:59:27 -0500

    adding beep; stepwise is working

--- src/sketch.ino
+++ src/sketch.ino
@@ -6,40 +6,6 @@ int out = LOW;
 
 int change = 0;
 
-void setup()
-{
-    for (int i = 0; i < 8; i++) {
-        pinMode(i, OUTPUT);
-    }
-
-    Serial.begin(115200);
-    Serial.setTimeout(10);
-}
-
-unsigned long freq_to_micros(float freq) {
-    return (1000.0 / freq) * 1000.0;
-}
-
-// takes a pin and a step and returns 0 or 1 for whether that pin is active on that step
-int activepin(int pin, int step) {
-    int pins[8] = {1,2,4,8,16,32,64,128};
-    int response = pins[pin] & step;
-    return response;
-}
-
-// takes a step and returns an array of pins with active or not on them
-int * activepins(int step) {
-    int output[8];
-    for (int i = 0; i < 8; i++) {
-        output[i] = activepin(i,step);
-    }
-    return output;
-}
-
-void reset_interval() {
-    interval = freq_to_micros(Serial.parseFloat());
-}
-
 void square() {
 
     if (Serial.available()) {
@@ -64,15 +30,62 @@ void square() {
     }
 
 }
+void beep() {
+    int now = millis();
+    while (millis() < now + 250) {
+        square();
+    }
+}
+
+void setup()
+{
+    for (int i = 0; i < 8; i++) {
+        pinMode(i, OUTPUT);
+    }
+
+    Serial.begin(115200);
+    Serial.setTimeout(10);
+    beep();
+}
+
+unsigned long freq_to_micros(float freq) {
+    return (1000.0 / freq) * 1000.0;
+}
+
+// takes a pin and a step and returns 0 or 1 for whether that pin is active on that step
+int activepin(int pin, int step) {
+    int pins[8] = {1,2,4,8,16,32,64,128};
+    int response = pins[pin] & step;
+    if (response != 0) {
+        return 1;
+    } else {
+        return 0;
+    }
+}
+
+// takes a step and returns an array of pins with active or not on them
+int * activepins(int step) {
+    int output[8];
+    for (int i = 0; i < 8; i++) {
+        output[i] = activepin(i,step);
+    }
+    return output;
+}
+
+void reset_interval() {
+    interval = freq_to_micros(Serial.parseFloat());
+}
+
 
 unsigned long sample_interval = freq_to_micros(44100.0);
 unsigned long sine_interval = freq_to_micros(440.0);
 unsigned long samplePreviousMicros = 0;
 int step = 0;
-int step_wise = 0;
+int step_wise = 1;
 int* binactivearray;
 
 void sine() {
+
     if (step == 0) {
         step_wise = 1;
     } else if (step == 255) {
@@ -80,13 +93,17 @@ void sine() {
     }
 
     currentMicros = micros();
-
     if (currentMicros - samplePreviousMicros > sample_interval) {
         samplePreviousMicros = micros();
-        if (currentMicros - previousMicros > (sample_interval / 255)) {
-
+        if (currentMicros - previousMicros > (sine_interval / 255)) {
             previousMicros = micros();
+
             binactivearray = activepins(step);
+
+            for (int i = 0; i < 8; i += 1) {
+                Serial.print(binactivearray[i]);
+            }
+            Serial.print('\n');
 
             for (int i = 0; i < 8; i++) {
                 if (binactivearray[i]) {
@@ -96,12 +113,13 @@ void sine() {
                 }
             }
         }
-        step += step_wise;
     }
+    step += step_wise;
 }
 
 
 void loop()
 {
+    // square();
     sine();
 }
